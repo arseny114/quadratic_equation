59ad1d423de1bb49b40a0cbd3208da453ef3131c
Arseny Kositsin <arseny.post@gmail.com>
2024-04-07 02:03:43 +0300

    Added comments to the quadratic_equation.c file

--- src/quadratic_equation.c
+++ src/quadratic_equation.c
@@ -1,48 +1,114 @@
+/*-------------------------------------------------------------
+ *
+ * This file contains the implementation of
+ * the quadratic_equation function.
+ *
+ * The function solves a quadratic equation
+ * and returns the msg_id characterizing the execution
+ * of the function. The msg_id definitions are in
+ * the quadratic_equation.h file.
+ *
+ * In addition, the file contains overflow checking
+ * functions (check_overflow) and a function for decrypting
+ * msg_id (get_solve_equation_msg).
+ *
+-------------------------------------------------------------*/
+
 #include <assert.h>
 #include <float.h>
 #include <math.h>
 #include <quadratic_equation.h>
 #include <stdbool.h>
 
+/*
+ * The function check whether the res value
+ * is outside the double range.
+ */
 static int check_overflow(long double res);
 
+/*
+ * The function returns a pointer to a string
+ * that decrypts the passed msg_id.
+ */
+char *get_solve_equation_msg(int msg_id);
+
+/*
+ * Implementation of the quadratic_equation function that solves
+ * the quadratic equation.
+ *
+ * If the equation has two different roots, they will be written
+ * in res1 and res2. If the equation has one root, then it will be
+ * written to both res1 and res2.
+ *
+ * If there are no roots, or the roots are infinite,
+ * or an overflow has occurred, the standard value is written to
+ * res1 and res2, which is set in the quadratic_equation.h file.
+ */
 int solve_equation(double a, double b, double c, double *res1, double *res2) {
 
+  /*
+   * Variables to be used in calculations. The long double data type
+   * is selected in order to eliminate double type overflow. At the
+   * time of writing the response to res1 and res2,
+   * an overflow check will be performed.
+   */
   long double discriminant, _a, _b, _c;
   long double _res1, _res2;
-  // int msg_id;
 
+  /* Checking pointers for a non-NULL value. */
   assert(res1);
   assert(res2);
 
+  /*
+   * The function writes the standard values to res1 and res2.
+   * Under some conditions described above, these values will
+   * not be changed.
+   */
   *res1 = *res2 = QE_STD_VAL_RES;
 
+  /* Checking for the infinity of roots. */
   if ((a == 0) && (b == 0) && (c == 0)) {
     return QE_OK_INF_RES;
 
+    /* If only `c` is not zero, the equation has no roots. */
   } else if ((a == 0) && (b == 0) && (c != 0)) {
     return QE_OK_NO_RES;
 
+    /* If only `b` is not zero, the equation has one root, res = 0. */
   } else if ((a == 0) && (b != 0) && (c == 0)) {
     *res1 = *res2 = 0;
     return QE_OK_ONE_RES;
 
+    /* If only `a` is not zero, the equation has one root, res = 0. */
   } else if ((a != 0) && (b == 0) && (c == 0)) {
     *res1 = *res2 = 0;
     return QE_OK_ONE_RES;
 
+    /*
+     * If only `a` is zero, then the equation will
+     * be solved using the simple formula given below.
+     */
   } else if ((a == 0) && (b != 0) && (c != 0)) {
     _b = b;
     _c = c;
 
     _res1 = -_c / _b;
 
+    /*
+     * Before writing the root to res1 and res2, an overflow
+     * check is performed. In case of overflow, the function
+     * returns the msg_id characterizing the overflow.
+     */
     if (check_overflow(_res1)) {
       *res1 = *res2 = _res1;
       return QE_OK_ONE_RES;
     } else
       return QE_ERR_OVERFLOW;
 
+    /*
+     * In all other cases, the equation is solved
+     * through the discriminant.
+     */
   } else {
 
     _a = a;
@@ -51,10 +117,19 @@ int solve_equation(double a, double b, double c, double *res1, double *res2) {
 
     discriminant = _b * _b - 4 * _a * _c;
 
+    /*
+     * Solving the equation if the discriminant
+     * is greater than zero.
+     */
     if (discriminant > 0) {
       _res1 = (-_b + sqrt(discriminant)) / (2 * _a);
       _res2 = (-_b - sqrt(discriminant)) / (2 * _a);
 
+      /*
+       * Before writing the root to res1 and res2, an overflow
+       * check is performed. In case of overflow, the function
+       * returns the msg_id characterizing the overflow.
+       */
       if (check_overflow(_res1) && check_overflow(_res2)) {
         *res1 = _res1;
         *res2 = _res2;
@@ -62,10 +137,16 @@ int solve_equation(double a, double b, double c, double *res1, double *res2) {
       } else
         return QE_ERR_OVERFLOW;
 
+      /* Solving the equation if the discriminant is zero. */
     } else if (discriminant == 0) {
       _res1 = (-_b) / (2 * _a);
       _res2 = _res1;
 
+      /*
+       * Before writing the root to res1 and res2, an overflow
+       * check is performed. In case of overflow, the function
+       * returns the msg_id characterizing the overflow.
+       */
       if (check_overflow(_res1)) {
         *res1 = _res1;
         *res2 = *res1;
@@ -73,12 +154,19 @@ int solve_equation(double a, double b, double c, double *res1, double *res2) {
       } else
         return QE_ERR_OVERFLOW;
 
+      /*
+       * If the discriminant is less than zero,
+       * the equation has no roots.
+       */
     } else
       return QE_OK_NO_RES;
   }
 }
 
-// Только для мейна
+/*
+ * The function returns a pointer to a string
+ * that decrypts the passed msg_id.
+ */
 char *get_solve_equation_msg(int msg_id) {
   switch (msg_id) {
 
@@ -103,6 +191,10 @@ char *get_solve_equation_msg(int msg_id) {
   }
 }
 
+/*
+ * The function check whether the res value
+ * is outside the double range.
+ */
 static int check_overflow(long double res) {
 
   if ((res > DBL_MAX) || (res < -DBL_MAX))
